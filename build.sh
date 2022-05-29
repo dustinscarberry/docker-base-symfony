003a98446eea338c89f81218c34dc7b2e77cdbf4
Dustin Scarberry <bitsnbytes1001@gmail.com>
2022-05-28 23:12:53 -0400

    Optimize build

--- build.sh
+++ build.sh
@@ -8,15 +8,15 @@ docker login
 # build and push base docker images
 
 # base images
-docker build -t dustinscarberry/symfony-base:php8.0 --build-arg PHP_VERSION=$php8Version -f 8.0/base/Dockerfile .
+docker build -t dustinscarberry/symfony-base:php8.0 --build-arg PHP_VERSION=$php8Version -f images/base/Dockerfile .
 docker push dustinscarberry/symfony-base:php8.0
 
-docker build -t dustinscarberry/symfony-base:php8.1 --build-arg PHP_VERSION=$php81Version -f 8.1/base/Dockerfile .
+docker build -t dustinscarberry/symfony-base:php8.1 --build-arg PHP_VERSION=$php81Version -f images/base/Dockerfile .
 docker push dustinscarberry/symfony-base:php8.1
 
 # extra images
-docker build -t dustinscarberry/symfony-base:php8.0-extra --build-arg PHP_VERSION=$php8Version -f 8.0/extra/Dockerfile .
+docker build -t dustinscarberry/symfony-base:php8.0-extra --build-arg PHP_VERSION=$php8Version -f images/extra/Dockerfile .
 docker push dustinscarberry/symfony-base:php8.0-extra
 
-docker build -t dustinscarberry/symfony-base:php8.1-extra --build-arg PHP_VERSION=$php81Version -f 8.1/extra/Dockerfile .
+docker build -t dustinscarberry/symfony-base:php8.1-extra --build-arg PHP_VERSION=$php81Version -f images/extra/Dockerfile .
 docker push dustinscarberry/symfony-base:php8.1-extra
