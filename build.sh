6ec53c7a71bbb5edd74c27360569d3405d55740f
Dustin Scarberry <bitsnbytes1001@gmail.com>
2022-07-11 11:54:14 -0400

    PHP version

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-php8Version=php:8.0.20-fpm-alpine3.16
-php81Version=php:8.1.7-fpm-alpine3.16
+php8Version=php:8.0.21-fpm-alpine3.16
+php81Version=php:8.1.8-fpm-alpine3.16
 
 docker login
 
