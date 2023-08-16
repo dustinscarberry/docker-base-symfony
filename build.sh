adc035006c30ca1cedbf01dd48d0e7d4dea8f67f
Dustin Scarberry <bitsnbytes1001@gmail.com>
2023-08-16 15:06:28 -0400

    Remove PHP 8.0 docker images

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-php8Version=php:8.0.30-fpm-alpine
 php81Version=php:8.1.22-fpm-alpine
 php82Version=php:8.2.8-fpm-alpine
 
@@ -9,9 +8,6 @@ docker login
 # build and push base docker images
 
 # base images
-docker build -t dustinscarberry/symfony:php8.0 --build-arg PHP_VERSION=$php8Version -f images/base/Dockerfile .
-docker push dustinscarberry/symfony:php8.0
-
 docker build -t dustinscarberry/symfony:php8.1 --build-arg PHP_VERSION=$php81Version -f images/base/Dockerfile .
 docker push dustinscarberry/symfony:php8.1
 
@@ -19,9 +15,6 @@ docker build -t dustinscarberry/symfony:php8.2 --build-arg PHP_VERSION=$php82Ver
 docker push dustinscarberry/symfony:php8.2
 
 # extra images
-docker build -t dustinscarberry/symfony:php8.0-extra --build-arg PHP_VERSION=$php8Version -f images/extra/Dockerfile .
-docker push dustinscarberry/symfony:php8.0-extra
-
 docker build -t dustinscarberry/symfony:php8.1-extra --build-arg PHP_VERSION=$php81Version -f images/extra/Dockerfile .
 docker push dustinscarberry/symfony:php8.1-extra
 
