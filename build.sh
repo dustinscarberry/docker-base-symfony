bbd155fbdbe55fab7ebd9ead5d5bcb74d9140ea8
Dustin Scarberry <bitsnbytes1001@gmail.com>
2024-11-25 16:06:55 -0500

    PHP 8.4 / version updates

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-php81Version=php:8.1.30-fpm-alpine
-php82Version=php:8.2.24-fpm-alpine
-php83Version=php:8.3.12-fpm-alpine
+php81Version=php:8.1.31-fpm-alpine
+php82Version=php:8.2.26-fpm-alpine
+php83Version=php:8.3.14-fpm-alpine
+php84Version=php:8.4.1-fpm-alpine
 
 docker login
 
@@ -15,8 +16,11 @@ docker push dustinscarberry/symfony:php8.1
 docker buildx build --platform linux/amd64 -t dustinscarberry/symfony:php8.2 --build-arg PHP_VERSION=$php82Version -f images/base/Dockerfile .
 docker push dustinscarberry/symfony:php8.2
 
-docker buildx build --platform linux/amd64 -t dustinscarberry/symfony:php8.3 --build-arg PHP_VERSION=$php83Version -f images/8.3-base/Dockerfile .
+docker buildx build --platform linux/amd64 -t dustinscarberry/symfony:php8.3 --build-arg PHP_VERSION=$php83Version -f images/base/Dockerfile .
 docker push dustinscarberry/symfony:php8.3
+
+docker buildx build --platform linux/amd64 -t dustinscarberry/symfony:php8.4 --build-arg PHP_VERSION=$php84Version -f images/base/Dockerfile .
+docker push dustinscarberry/symfony:php8.4
 
 # extra images
 docker buildx build --platform linux/amd64 -t dustinscarberry/symfony:php8.1-extra --build-arg PHP_VERSION=$php81Version -f images/extra/Dockerfile .
@@ -25,5 +29,8 @@ docker push dustinscarberry/symfony:php8.1-extra
 docker buildx build --platform linux/amd64 -t dustinscarberry/symfony:php8.2-extra --build-arg PHP_VERSION=$php82Version -f images/extra/Dockerfile .
 docker push dustinscarberry/symfony:php8.2-extra
 
-docker buildx build --platform linux/amd64 -t dustinscarberry/symfony:php8.3-extra --build-arg PHP_VERSION=$php83Version -f images/8.3-extra/Dockerfile .
+docker buildx build --platform linux/amd64 -t dustinscarberry/symfony:php8.3-extra --build-arg PHP_VERSION=$php83Version -f images/extra/Dockerfile .
 docker push dustinscarberry/symfony:php8.3-extra
+
+docker buildx build --platform linux/amd64 -t dustinscarberry/symfony:php8.4-extra --build-arg PHP_VERSION=$php84Version -f images/extra/Dockerfile .
+docker push dustinscarberry/symfony:php8.4-extra
